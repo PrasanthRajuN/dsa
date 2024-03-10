bc299b30dccf0c4e01b452fba7cccd541e9e3ea7
PrasanthRajuN <159819986+PrasanthRajuN@users.noreply.github.com>
2024-03-10 13:24:10 +0530

    Add files via upload

---
+++ Greedy.java
@@ -0,0 +1,103 @@
+import java.util.*;
+import java.util.Arrays;
+
+public class Greedy {
+    static class Item {
+        int value, weight;
+
+        Item(int value, int weight) {
+            this.value = value;
+            this.weight = weight;
+        }
+    }
+
+    static void fractionalknapsack(int W, Item ar[], int n) {
+        double[][] a = new double[n][3];
+        double w = W;
+        for (int i = 0; i < n; i++) {
+            a[i][0] = ar[i].weight;
+            a[i][1] = ar[i].value;
+            a[i][2] = ar[i].value / ar[i].weight;
+        }
+        Arrays.sort(a, Comparator.comparingDouble(o -> o[2]));
+        double profit = 0.0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (w >= a[i][0]) {
+                profit += a[i][1];
+                w -= a[i][0];
+            } else {
+                profit += (a[i][1] / a[i][0]) * w;
+                break;
+            }
+        }
+        System.out.print(profit);
+    }
+
+    static void knapscak() {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int w = s.nextInt();
+        Item[] ar = new Item[n];
+        for (int i = 0; i < n; i++) {
+            ar[i] = new Item(s.nextInt(), s.nextInt());
+        }
+        fractionalknapsack(w, ar, n);
+    }
+
+    static class Job {
+        int job, deadline, profit;
+
+        Job(int job, int deadline, int profit) {
+            this.job = job;
+            this.deadline = deadline;
+            this.profit = profit;
+        }
+    }
+
+    static void JobScheduling(Job arr[], int n) {
+        int[][] a = new int[n][2];
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            a[i][0] = arr[i].deadline;
+            a[i][1] = arr[i].profit;
+            max = Math.max(max, a[i][0]);
+        }
+        int[] size = new int[max];
+        int[] ans = new int[2];
+        Arrays.sort(a, Comparator.comparingDouble(o -> o[1]));
+        for (int i = n - 1; i >= 0; i--) {
+            int ind = a[i][0] - 1;
+            int profit = a[i][1];
+            if (size[ind] == 0) {
+                size[ind] = -1;
+                ans[0] += 1;
+                ans[1] += profit;
+            } else {
+                while (ind >= 0) {
+                    if (size[ind] == 0) {
+                        size[ind] = -1;
+                        ans[0] += 1;
+                        ans[1] += profit;
+                        break;
+                    }
+                    ind--;
+                }
+            }
+        }
+        System.out.println(ans[0] + " " + ans[1]);
+    }
+
+    static void jobmain() {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        Job[] arr = new Job[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = new Job(s.nextInt(), s.nextInt(), s.nextInt());
+        }
+        JobScheduling(arr, n);
+    }
+
+    public static void main(String args[]) {
+        jobmain();
+    }
+}
